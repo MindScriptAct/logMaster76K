636dbefb64a4231fafcf2188c48579b931b00eee
MindScriptAct <raima156@yahoo.com>
2013-06-20 11:46:15 +0200

    Renamed, downloads added.

---
+++ LogMaster_API/bildSWC.bat
@@ -0,0 +1,3 @@
+cd C:\Users\rbanevicius\Dropbox\!intelliSpace\logMaster\LogMasterAPI
+C:\flex_sdk_4.9.1\bin\compc.bat      -target-player=10.0      -debug=false   -framework=C:\flex_sdk_4.9.1\frameworks      -source-path=src      -library-path=C:\flex_sdk_4.9.1\frameworks\libs\player\10.0\playerglobal.swc      -include-sources=src               -output=bin\LagMasterAPI_v1_0.swc
+pause
